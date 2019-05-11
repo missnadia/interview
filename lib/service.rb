246d00d8712449501e1b57dbcc786c65e12fc706
learn <miss.nadia.lee@gmail.com>
2019-05-10 22:18:13 -0700

    Add select_service method in CLI and Service; refactor all_user_routes in Service

--- lib/service.rb
+++ lib/service.rb
@@ -20,6 +20,8 @@ class Service
 
     ALL_SERVICES = STRAVA + RWGPS + KOMOOT
 
+    USER = []
+
     def all_service_routes
         puts "#{ALL_SERVICES}"
     end
@@ -30,9 +32,60 @@ class Service
     end
 
     def all_user_routes(user_id)
-        
+        strava_user(user_id)
+        rwgps_user(user_id)
+        komoot_user(user_id)
+        puts "#{USER.flatten}"
     end
 
-    def routes_by_service(user_id)
+    def routes_by_service(user_id, input)
+        case input
+        when 1
+            strava_user(user_id)
+            puts "#{USER.flatten}"
+            puts ""
+        when 2
+            rwgps_user(user_id)
+            puts "#{USER.flatten}"
+            puts ""
+        when 3
+            komoot_user(user_id)
+            puts "#{USER.flatten}"
+            puts ""
+        when 4
+            strava_user(user_id)
+            rwgps_user(user_id)
+            puts "#{USER.flatten}"
+            puts ""
+        when 5
+            strava_user(user_id)
+            komoot_user(user_id)
+            puts "#{USER.flatten}"
+            puts ""
+        when 6
+            komoot_user(user_id)
+            rwgps_user(user_id)
+            puts "#{USER.flatten}"
+            puts ""
+        when 7
+            puts "Goodbye."
+            input == "exit"
+        else
+            puts "Invalid entry."
+            puts ""
+            start
+        end
+    end
+
+    def strava_user(user_id)
+        USER << STRAVA.map { |route| "#{user_id}" + "#{route}" }
+    end
+
+    def rwgps_user(user_id)
+        USER << RWGPS.map { |route| "#{route}" + "#{user_id}" }
+    end
+
+    def komoot_user(user_id)
+        USER << KOMOOT.map { |route| "#{user_id}" + "#{route}" + "#{user_id}" }
     end
 end
--- lib/services/cli.rb
+++ lib/services/cli.rb
@@ -69,8 +69,43 @@ class CLI < Service
         when 1
             all_user_routes(user_id)
         when 2
-            routes_by_service(user_id)
+            select_service(user_id)
         when 3
+            puts "Goodbye."
+            input == "exit"
+        else
+            puts "Invalid entry."
+            puts ""
+            start
+        end
+    end
+
+    def select_service(user_id)
+        puts "Please select from the following service(s):"
+        puts "1. Strava"
+        puts "2. RWGPS"
+        puts "3. Komoot"
+        puts "4. Strava, RWGPS"
+        puts "5. Strava, Komoot"
+        puts "6. Komoot, RWGPS"
+        puts "7. Exit"
+        puts ""
+        input = gets.strip.to_i
+        puts ""
+        routes_by_service(user_id, input)
+        restart
+    end
+
+    def restart
+        puts "Please select from the following:"
+        puts "1. Return to Main Menu"
+        puts "2. Exit"
+        input = gets.strip.to_i
+        puts ""
+        case input
+        when 1
+            start
+        when 2
             puts "Goodbye."
             input == "exit"
         else
