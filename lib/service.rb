883c52068b226bd29f09b3713f5882b2181881a7
learn <miss.nadia.lee@gmail.com>
2019-05-10 19:32:35 -0700

    Create Service class; add services routes

---
+++ lib/service.rb
@@ -0,0 +1,20 @@
+class Service
+
+    STRAVA = [
+        "SRT", 
+        "CVT", 
+        "Perkiomen"
+    ]
+
+    RWGPS = [
+        "CVT", 
+        "Perkiomen", 
+        "Welsh Mountain"
+    ]
+
+    KOMOOT = [
+        "SRT", 
+        "Welsh Mountain", 
+        "Oaks to Philly"
+    ]
+end
